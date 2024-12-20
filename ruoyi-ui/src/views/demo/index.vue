e97378a323b9f1cc970f28999b3fa044cbf3d6fb
Lin <ljw636@126.com>
2024-12-21 00:45:19 +0800

    feat: dynamic add hide style onDesign

--- ruoyi-ui/src/views/demo/index.vue
+++ ruoyi-ui/src/views/demo/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <Designer :template="template" @onDesigned="onDesigned" />
+  <Designer
+    :autoConnect="false"
+    :template="template"
+    @onDesigned="onDesigned"
+  />
 </template>
 
 <script>
@@ -8,12 +12,61 @@ import { Designer } from "@sv-print/vue";
 export default {
   components: { Designer },
   data() {
-    return { template: {} };
+    return {
+      templateId: null,
+      hideStyleElement: null,
+      template: {},
+    };
+  },
+  watch: {
+    templateId(val) {
+      if (val) {
+        console.log("templateId changed", val);
+        this.hideWatermark();
+      }
+    },
+  },
+  mounted() {
+    this.hideWatermark();
+  },
+  beforeDestroy() {
+    // 直接移除之前添加的 <style> 标签（如果有）
+    if (this.hideStyleElement) {
+      document.head.removeChild(this.hideStyleElement);
+    }
   },
   methods: {
+    hideWatermark() {
+      if (!this.templateId) return;
+
+      // 如果已经存在旧的隐藏水印样式，则先移除它
+      if (this.hideStyleElement) {
+        document.head.removeChild(this.hideStyleElement);
+      }
+
+      // 添加隐藏水印和其他元素的样式
+      const hideStyle = document.createElement("style");
+      hideStyle.id = "hide-style";
+      hideStyle.innerHTML = `
+        div[class*="${this.templateId}"],
+        .hiprint-printPaper-background,
+        #dragBox-rotateTools,
+        #SVPrint .svp-footer {
+          display: none !important;
+        }
+      `;
+      document.head.appendChild(hideStyle);
+
+      console.log("style added", hideStyle);
+    },
     onDesigned(e) {
-      console.log("designer ", e);
-      console.log("hiprint ", e.hiprint); // hiprint 模块
+      const designer = e.detail;
+      console.log("designer ", designer);
+      console.log("designerUtils ", designer.designerUtils);
+      console.log("hiprint ", designer.hiprint);
+      console.log("printTemplate ", designer.printTemplate);
+
+      this.templateId = designer.printTemplate.id;
     },
   },
 };
